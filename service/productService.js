da0242660ef04c3ea1d6b063ed9edd0cf9a435bc
protonsDev <shubham.joshi@protonshub.in>
2025-04-09 22:48:42 +0530

    throw error

--- service/productService.js
+++ service/productService.js
@@ -5,6 +5,6 @@ export const addProductInDB = async (data) => {
     const product = new Product(data);
     return await product.save();
   } catch (error) {
-    return error;
+    throw error;
   }
 };
